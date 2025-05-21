e8dc348d7bd0ce2329a6a0458ed20aa711a60abb
jcvalladares <juan.valladares@americasgroup.net>
2025-05-21 10:30:53 -0500

    Update AppDelegate.swift

--- prototypes/IOS/communities/communities/AppDelegate.swift
+++ prototypes/IOS/communities/communities/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//document this code
+//todo: document this code
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
